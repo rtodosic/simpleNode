55ff55dcd11be41a5246d8397dd2af3ae98417bb
Richard Todosichuk <rtodosic@hotmail.com>
2018-07-02 21:14:44 -0400

    Fixed sp errors

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ var server = http.createServer(function(request, response) {
 response.writeHead(200, {"Content-Type": "text/html"});
 response.write("<!DOCTYPE html>");
 response.write("<html>");
-response.write(`<head><title>${environment}</title></head>`);
+response.write(`<head><title>Environment</title></head>`);
 response.write("<body>");
-response.write(`<p>Enviroment = ${environment}</p>`);
+response.write(`<p>Environment: ${environment}</p>`);
 response.write("</body>");
 response.write("</html>");
 response.end();
